3dfa74bfabd4e8d5d19413bb639be25b98bff506
SubtlePanzer <rdl132345@gmail.com>
2026-08-14 11:19:23 +0930

    Page works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,7 +10,6 @@ def launch():
 def index_page():
         # homepage
         return render_template('index.html')
-
 
 
 launch()
